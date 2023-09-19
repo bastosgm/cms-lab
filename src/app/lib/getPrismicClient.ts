9d3d14ae7f434705ae0c579ad40f5ba0e07b61d5
Gustavo Bastos <bastosgm@outlook.com>
2023-09-19 06:29:29 -0300

    build(WIP): configured prismic to get home page info

--- src/app/lib/getPrismicClient.ts
+++ src/app/lib/getPrismicClient.ts
@@ -1,12 +1,48 @@
 import Prismic from "@prismicio/client";
+import * as RichText from "@prismicio/richtext";
 import { createClient } from "@/prismicio";
+
+export interface Content {
+  title: string;
+  subtitle: string;
+  linkAction: string;
+  firstSectionTitle: string;
+  firstSectionDescription: string;
+  firstSectionImage: string;
+  secondSectionTitle: string;
+  secondSectionDescription: string;
+  secondSectionImage: string;
+}
 
 export default async function getPrismicClient() {
   const client = createClient();
 
   // TODO: resolver o erro de type "not page" que não faz muito sentido aparentemente
-  const page = await client.getSingle("home");
+  const {
+    title,
+    subtitle,
+    link_action,
+    first_section_title,
+    first_section_description,
+    first_section_image,
+    second_section_title,
+    second_section_description,
+    second_section_image,
+  } = await client.getSingle("home").then((r) => r.data as any);
 
-  console.log(page);
-  return page;
+  const content: Content = {
+    title: RichText.asText(title),
+    subtitle: RichText.asText(subtitle),
+    linkAction: link_action.url,
+    firstSectionTitle: RichText.asText(first_section_title),
+    firstSectionDescription: RichText.asText(first_section_description),
+    firstSectionImage: first_section_image.url,
+    secondSectionTitle: RichText.asText(second_section_title),
+    secondSectionDescription: RichText.asText(second_section_description),
+    secondSectionImage: second_section_image.url,
+  };
+
+  console.log(content);
+
+  return content;
 }
