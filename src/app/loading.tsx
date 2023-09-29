2349e0885f008f1fc65d40c3add2813601f97829
Gustavo Bastos <bastosgm@outlook.com>
2023-09-29 05:32:54 -0300

    refactor: updated some components from client side to server side with loading component

---
+++ src/app/loading.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+
+//TODO: só funciona esse cara se o componente for SSR
+export default function Loading() {
+  return <h1 style={{ color: "red" }}>carregando...</h1>;
+}
